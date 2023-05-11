8192b40a66f74f50a8f1b4882e00000b3f934a18
Darshna76 <117654044+Darshna76@users.noreply.github.com>
2023-05-11 14:59:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let radius =parseInt(prompt());
+let area=3.14159265359*radius*radius;
+const num = area.toFixed(2);
+console.log(`The area of the circle with radius ${radius} is ${num}`)
